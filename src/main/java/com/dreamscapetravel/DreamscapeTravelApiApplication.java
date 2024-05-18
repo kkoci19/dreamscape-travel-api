36b5b896c534103970fe60a6ab4fbe3511f5af61
Klaus <klauskoci@gmail.com>
2024-05-18 10:50:16 +0200

    first commit

---
+++ src/main/java/com/dreamscapetravel/DreamscapeTravelApiApplication.java
@@ -0,0 +1,13 @@
+package com.dreamscapetravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamscapeTravelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamscapeTravelApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreamscapetravel/DreamscapeTravelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamscapetravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamscapeTravelApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
